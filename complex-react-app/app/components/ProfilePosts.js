d6778b6633b22061d90043f7d6410857d1160ab5
Hostnomics <hostnomics@gmail.com>
2023-09-13 23:56:30 -0400

    Added react-router-dom Link to ProfilePosts.js and added a link to each logged in users post

--- complex-react-app/app/components/ProfilePosts.js
+++ complex-react-app/app/components/ProfilePosts.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react"
 
-import {useParams} from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 import Axios from 'axios'
 
 function ProfilePosts() {
@@ -40,10 +40,10 @@ function ProfilePosts() {
             const date = new Date(post.createdDate)
             const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
             return (
-                <a key={post._id} href="#" className="list-group-item list-group-item-action">
+                <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
                     <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {" "}
                     <span className="text-muted small">on {dateFormatted} </span>
-                </a>
+                </Link>
             )
 
         })}
